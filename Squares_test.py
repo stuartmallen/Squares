61f10aed207dbe002a622cd161d9430bf9d49847
stuartmallen <AllenSM@cardiff.ac.uk>
2024-09-11 16:31:04 +0100

    Debugging

--- Squares_test.py
+++ Squares_test.py
@@ -1,8 +1,7 @@
 import Squares
 
-class TestClass:
-    def test_one(self):
-        assert Squares.square(2) == 4
+def test_one():
+    assert Squares.square(2) == 4
 
-    def test_two(self):
-        Squares.square(-1) == 1
+def test_two():
+    Squares.square(-1) == 1
